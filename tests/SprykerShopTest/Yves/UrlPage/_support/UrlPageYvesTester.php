566a52f5426a3047c63dce2c1b22a0d3453e3b99
Artem Siminenko <artem.siminenko@gmail.com>
2023-10-06 11:11:10 +0200

    CC-31029 Fixed exception for deactivated products in cart.  (#2398)
    CC-31029 Fixed exception for deactivated products in cart.

---
+++ tests/SprykerShopTest/Yves/UrlPage/_support/UrlPageYvesTester.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShopTest\Yves\UrlPage;
+
+use Codeception\Actor;
+
+/**
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = null)
+ *
+ * @SuppressWarnings(\SprykerShopTest\Yves\UrlPage\PHPMD)
+ */
+class UrlPageYvesTester extends Actor
+{
+    use _generated\UrlPageYvesTesterActions;
+}
